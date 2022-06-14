ee6131a40da847458ac3f0949cbc9dac2e3128f2
NightMachinery <rudiwillalwaysloveyou@gmail.com>
2022-06-14 17:32:34 +0430

    updated build config

--- build.clj
+++ build.clj
@@ -1,10 +1,21 @@
 (ns build
   (:refer-clojure :exclude [test])
+  (:require [clojure.tools.build.api :as b])
   (:require [org.corfield.build :as bb]))
 
 (def lib 'net.clojars.NightMachinery/vcard-to-json)
 (def version "0.1.0-SNAPSHOT")
 (def main 'NightMachinery.vcard-to-json)
+
+(defn rebuild-uber
+  "Deletes the old artifacts and builds a new uberjar."
+  [opts]
+  (-> opts
+      (assoc :lib lib :version version :main main)
+      ;; (bb/run-tests)
+      (bb/clean)
+      (bb/uber))
+  )
 
 (defn test "Run the tests." [opts]
   (bb/run-tests opts))
@@ -15,3 +26,5 @@
       (bb/run-tests)
       (bb/clean)
       (bb/uber)))
+;;;
+;;;
