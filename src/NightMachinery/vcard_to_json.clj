faee7da3fa2cbcddfebe9e1d0751910443b46f62
NightMachinery <rudiwillalwaysloveyou@gmail.com>
2022-06-16 22:42:01 +0430

    changed the semantics of remove-nils. now using ordered-map. now supports raw/extended properties.

--- src/NightMachinery/vcard_to_json.clj
+++ src/NightMachinery/vcard_to_json.clj
@@ -3,37 +3,50 @@
   (:require [clojure.data.json :as json]))
 
 (import (ezvcard Ezvcard VCard))
+(use 'flatland.ordered.map)
 
 ;;;
 (defn remove-nils [m]
   (let [f (fn [x]
-            (cond
-              (map? x)
-              (let [kvs (filter
-                         (fn [x]
-                           (let [v (second x)]
-                             (cond
-                               (or
-                                (nil? v)
-                                (empty? v)) false
-                               :else true)))
-                         x)]
-                (if (empty? kvs) nil (into {} kvs)))
-              (coll? x)
-              (do
-                ;; (println x)
-                (let [res (vec (filter (comp not nil?) x))]
-                  ;; (when (not (empty? res)) res)
-                  res
-                  ))
-              :else x))]
-    (clojure.walk/postwalk f m)))
+            (do
+              ;; (println "x: " x)
+              (cond
+                (map? x)
+                (do
+                 ;; (println "map: " x)
+                 (let [kvs (filter
+                            (fn [x]
+                              (let [v (second x)]
+                                (cond
+                                  (or
+                                   (nil? v)
+                                   (and (coll? v) (empty? v))) false
+                                  :else true)))
+                            x)]
+                   (do
+                     ;; (println "kvs: " kvs)
+                     (if (empty? kvs) nil (into {} kvs)))))
+                (coll? x)
+                (do
+                  ;; (println "coll: " x)
+                  (when (some some? x)
+                    x))
+                :else
+                (do
+                  ;; (println "else: " x)
+                  x))))]
+    (->>
+     (clojure.walk/postwalk f m)
+     (filter some?))))
 
 (comment
-  (remove-nils {"a" [1 2 nil 3] "b" [nil]})
-  (remove-nils {"a" "j"})
-  (map? {"a" [1 2 nil 3]})
-  (coll? "ha"))
+  (remove-nils [{"a" [1 2 nil 3] "b" [nil nil] "c" [] "d" 9}])
+
+  (clojure.walk/postwalk #(do (println % 1) % 1)
+                         {"a" nil
+                          "b"
+                          {"c" nil}})
+  )
 ;;;
 (defmacro locals []
   (into {}
@@ -87,13 +100,36 @@ item2.X-ABLabel:_$!<Friend>!$_
 NOTE:some notes here
 BDAY:1958-11-14
 END:VCARD
+BEGIN:VCARD
+VERSION:3.0
+PRODID:-//Apple Inc.//macOS 11.2.1//EN
+N:;;;;
+FN:Apple Inc.
+ORG:Apple Inc.;
+TEL;type=MAIN;type=pref:1-800-MY-APPLE
+item1.ADR;type=WORK;type=pref:;;1 Infinite Loop;Cupertino;CA;95014;United States
+item1.X-ABADR:us
+item2.URL;type=pref:http://www.apple.com
+item2.X-ABLabel:_$!<HomePage>!$_
+X-ABShowAs:COMPANY
+END:VCARD
+BEGIN:VCARD
+VERSION:3.0
+PRODID:-//Apple Inc.//macOS 11.2.1//EN
+N:;;;;
+END:VCARD
 ")
-
 ;;;
 (defn greet
   "Callable entry point to the application."
   [data]
   (println (str "Hello, " (or (:name data) "World") "!")))
+;;;
+(defn json-dbg [x]
+  (do
+    ;; (println "DBG823")
+    (println (json/write-str x))
+    x))
 
 (defn -main
   "Reads vcards from the stdin and converts them to JSON."
@@ -110,39 +146,64 @@ END:VCARD
                     telephone-numbers (seq (.getTelephoneNumbers contact))
                     notes (.getNotes contact)]]
          ;; https://www.evenx.com/vcard-3-0-format-specification
-         ;;;
-         {:names-formatted (map #(.getValue %1)
-                                (.getFormattedNames contact))
-          :names-given (map #(.getGiven %1)
-                            structured-names)
-          :names-family (map #(.getFamily %1)
-                             structured-names)
-          :telephone-numbers (for [telephone-number telephone-numbers
-                                   :let [types
-                                         (seq (.getTypes telephone-number))]]
-                               {:types (map #(.getValue %1) types)
-                                :text (.getText telephone-number)
-                                })
-          :emails (for [email (.getEmails contact)
-                                   :let [types
-                                         (seq (.getTypes email))]]
-                               {:types (map #(.getValue %1) types)
-                                :value (.getValue email)
-                                })
-          :addresses (for [address (.getAddresses contact)
-                                   :let [types
-                                         (seq (.getTypes address))]]
-                               {:types (map #(.getValue %1) types)
-                                :countries (.getCountries address)
-                                :streetAddresses (.getStreetAddresses address)
-                                :extendedAddresses (.getExtendedAddresses address)
-                                :postalCodes (.getPostalCodes address)
-                                })
-          :birthdays (for [birthday (.getBirthdays contact)]
-                      (.getDate birthday))
-          :notes (map #(.getValue %1)
-                      notes)
-          })
+;;;
+         (->
+          (do
+            (ordered-map
+             :names-formatted (map #(.getValue %1)
+                                   (.getFormattedNames contact))
+
+             :names-given (map #(.getGiven %1)
+                               structured-names)
+
+             :names-family (map #(.getFamily %1)
+                                structured-names)
+
+             :telephone-numbers (for [telephone-number telephone-numbers
+                                      :let [types
+                                            (seq (.getTypes telephone-number))]]
+                                  {:types (map #(.getValue %1) types)
+                                   :text (.getText telephone-number)
+                                   })
+
+             :emails (for [email (.getEmails contact)
+                           :let [types
+                                 (seq (.getTypes email))]]
+                       {:types (map #(.getValue %1) types)
+                        :value (.getValue email)
+                        })
+
+             :addresses (for [address (.getAddresses contact)
+                              :let [types
+                                    (seq (.getTypes address))]]
+                          {:types (map #(.getValue %1) types)
+                           :countries (.getCountries address)
+                           :streetAddresses (.getStreetAddresses address)
+                           :extendedAddresses (.getExtendedAddresses address)
+                           :postalCodes (.getPostalCodes address)
+                           })
+
+             :birthdays (for [birthday (.getBirthdays contact)]
+                          (.getDate birthday))
+
+             :notes (map #(.getValue %1)
+                         notes)
+
+             :other-properties (for [p (.getExtendedProperties contact)]
+                         {
+                          :parameters
+                          (.asMap (.getParameters p))
+                          :property-name
+                          (.getPropertyName p)
+                          :group
+                          (.getGroup p)
+                          :value
+                          (.getValue p)
+                          })
+             ))
+          ;; json-dbg
+          ;; (remove-nils)
+          ))
        (remove-nils)
        (json/write-str)
        (println)
@@ -150,11 +211,15 @@ END:VCARD
 
       (comment
         (def c (nth contacts 0))
+        (def contact (nth contacts 0))
         (bean c)
         (filter
          (fn [p]
            (instance? p ezvcard.property.RawProperty))
          (seq (.getProperties c)))
+        (.asMap (.getParameters
+                 (first (.getExtendedProperties c))))
+        (.getGroup (first (.getExtendedProperties c)))
         (.getRelations c)
         (.getAddresses c)
         (.getEmails c)
